5afa2f79f21d25f693f2c13a0d4ba496290eb790
Alexis Gutiérrez <agutierrez@tecnoaccion.corp>
2022-12-13 14:52:04 -0300

    agrego boton de clonar y hago get de los repos

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
 import logo from './logo.svg';
-import Octokit from 'octokit'
+
+import axios from 'axios'
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [load, setLoad] = useState(false)
+  const [repos, setRepos] = useState([])
 
-  const octokit = new Octokit({
-    auth: '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'
-  });
+
+  const getRepos = async () => {
+    try {
+      const response = await axios.get("https://api.github.com/orgs/frontendMentor-me/repos")
+      if (response.status === 200) {
+        setLoad(true)
+        setRepos(response.data)
+        console.log(response.data)
+      }
+    } catch (error) {
+      console.log("error: ", error.message)
+    }
+  }
+  const handleClick = (index) => {
+    const asd = document.getElementsByClassName("url_repo")[index].innerHTML
+
+    var aux = document.createElement("input");
+    aux.setAttribute("value", asd);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+  }
+  if (!load) {
+    getRepos()
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Frontendmentor repositories</h1>
+      <div className='repositories'>
+        {repos &&
+          repos.map((repo, index) =>
+            <div className='repository' key={index}>
+
+              <h3>{repo.name}</h3>
+              <div className='description-repo'>
+                <button onClick={() => handleClick(index)}>Click to clone</button>
+                <p className='url_repo' style={{ display: "none" }}>{repo.clone_url}</p>
+              </div>
+            </div>
+          )
+        }
+      </div>
     </div>
   );
 }
